d96809f8bd9844d3acd0716c4f76127c8a03c879
shanks125 <sankalppateriya@gmail.com>
2023-04-01 20:28:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,124 +1,28 @@
-#!/usr/bin/env python
+Convert the below code so instead of user inputting the value for DEFAULT_BASE_MODEL_REPO and DEFAULT_BASE_MODEL_URL, there is a single drop-down menu with multiple options that user can select based on their preferences.
 
-from __future__ import annotations
+with gr.Accordion(label='Base model', open=False):
+        current_base_model = gr.Text(label='Current base model',
+                                     value=DEFAULT_BASE_MODEL_URL)
+        with gr.Row():
+            base_model_repo = gr.Text(label='Base model repo',
+                                      max_lines=1,
+                                      placeholder=DEFAULT_BASE_MODEL_REPO,
+                                      interactive=ALLOW_CHANGING_BASE_MODEL)
+            base_model_filename = gr.Text(
+                label='Base model file',
+                max_lines=1,
+                placeholder=DEFAULT_BASE_MODEL_FILENAME,
+                interactive=ALLOW_CHANGING_BASE_MODEL)
+        change_base_model_button = gr.Button('Change base model')
+        gr.Markdown(
+            '''- You can use other base models by specifying the repository name and filename.
+The base model must be compatible with Stable Diffusion v1.5.''')
 
-import os
-import pathlib
-import shlex
-import subprocess
-
-import gradio as gr
-
-if os.getenv('SYSTEM') == 'spaces':
-    with open('patch') as f:
-        subprocess.run(shlex.split('patch -p1'), stdin=f, cwd='ControlNet')
-
-base_url = 'https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/'
-names = [
-    'body_pose_model.pth',
-    'dpt_hybrid-midas-501f0c75.pt',
-    'hand_pose_model.pth',
-    'mlsd_large_512_fp32.pth',
-    'mlsd_tiny_512_fp32.pth',
-    'network-bsds500.pth',
-    'upernet_global_small.pth',
-]
-for name in names:
-    command = f'wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/{name} -O {name}'
-    out_path = pathlib.Path(f'ControlNet/annotator/ckpts/{name}')
-    if out_path.exists():
-        continue
-    subprocess.run(shlex.split(command), cwd='ControlNet/annotator/ckpts/')
-
-from gradio_canny2image import create_demo as create_demo_canny
-from gradio_depth2image import create_demo as create_demo_depth
-from gradio_fake_scribble2image import create_demo as create_demo_fake_scribble
-from gradio_hed2image import create_demo as create_demo_hed
-from gradio_hough2image import create_demo as create_demo_hough
-from gradio_normal2image import create_demo as create_demo_normal
-from gradio_pose2image import create_demo as create_demo_pose
-from gradio_scribble2image import create_demo as create_demo_scribble
-from gradio_scribble2image_interactive import \
-    create_demo as create_demo_scribble_interactive
-from gradio_seg2image import create_demo as create_demo_seg
-from model import (DEFAULT_BASE_MODEL_FILENAME, DEFAULT_BASE_MODEL_REPO,
-                   DEFAULT_BASE_MODEL_URL, Model)
-
-MAX_IMAGES = 4
-ALLOW_CHANGING_BASE_MODEL = 'hysts/ControlNet-with-other-models'
-
-model = Model()
-
-with gr.Blocks(css='style.css') as demo:
-    with gr.Tabs():
-        with gr.TabItem('Canny'):
-            create_demo_canny(model.process_canny, max_images=MAX_IMAGES)
-        with gr.TabItem('Hough'):
-            create_demo_hough(model.process_hough, max_images=MAX_IMAGES)
-        with gr.TabItem('HED'):
-            create_demo_hed(model.process_hed, max_images=MAX_IMAGES)
-        with gr.TabItem('Scribble'):
-            create_demo_scribble(model.process_scribble, max_images=MAX_IMAGES)
-        with gr.TabItem('Scribble Interactive'):
-            create_demo_scribble_interactive(
-                model.process_scribble_interactive, max_images=MAX_IMAGES)
-        with gr.TabItem('Fake Scribble'):
-            create_demo_fake_scribble(model.process_fake_scribble,
-                                      max_images=MAX_IMAGES)
-        with gr.TabItem('Pose'):
-            create_demo_pose(model.process_pose, max_images=MAX_IMAGES)
-        with gr.TabItem('Segmentation'):
-            create_demo_seg(model.process_seg, max_images=MAX_IMAGES)
-        with gr.TabItem('Depth'):
-            create_demo_depth(model.process_depth, max_images=MAX_IMAGES)
-        with gr.TabItem('Normal map'):
-            create_demo_normal(model.process_normal, max_images=MAX_IMAGES)
-
-import gradio as gr
-
-DEFAULT_BASE_MODEL_REPO = "default_repo"
-DEFAULT_BASE_MODEL_FILENAME = "default_filename"
-DEFAULT_BASE_MODEL_URL = "default_url"
-ALLOW_CHANGING_BASE_MODEL = True
-
-base_model_options = [
-    {'label': 'Option 1', 'value': {'base_model_repo': 'repo1', 'base_model_filename': 'filename1'}},
-    {'label': 'Option 2', 'value': {'base_model_repo': 'repo2', 'base_model_filename': 'filename2'}},
-    {'label': 'Option 3', 'value': {'base_model_repo': 'repo3', 'base_model_filename': 'filename3'}}
-]
-
-def placeholder_fn(repo_dropdown, base_model_filename):
-    # Placeholder function that simply returns the base model URL
-    base_model_url = f"https://github.com/{repo_dropdown['base_model_repo']}/{base_model_filename}"
-    return base_model_url
-
-interface = gr.Interface(
-    fn=placeholder_fn,
-    inputs=[
-        gr.Accordion(label='Base model', open=False, children=[
-            gr.Text(label='Current base model', value=DEFAULT_BASE_MODEL_URL),
-            gr.Row(children=[
-                gr.inputs.Dropdown(label='Base model repo', choices=base_model_options),
-                gr.Text(label='Base model file', value=DEFAULT_BASE_MODEL_FILENAME)
-            ]),
-            gr.Markdown(
-                '''- You can use other base models by specifying the repository name and filename.
-                The base model must be compatible with Stable Diffusion v1.5.'''
-            ),
-            gr.Button('Change base model')
-        ])
-    ],
-    outputs=gr.Textbox(label='Base model URL'),
-    title='Base Model Changer',
-    description='Change the base model for your application'
-)
-
-def set_base_model(repo_dropdown, base_model_filename):
-    # Code to set the base model using the selected option from the dropdown
-    pass
-
-interface.inputs[0][0][3].clicked(set_base_model)
-
-interface.launch()
+    change_base_model_button.click(fn=model.set_base_model,
+                                   inputs=[
+                                       base_model_repo,
+                                       base_model_filename,
+                                   ],
+                                   outputs=current_base_model)
 
 demo.queue(api_open=False).launch()
