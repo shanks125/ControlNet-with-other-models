f8d251dfbed0804023fa2ec1d790dc7b1672b8b2
shanks125 <sankalppateriya@gmail.com>
2023-04-01 15:54:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,9 +77,17 @@ with gr.Blocks(css='style.css') as demo:
     with gr.Accordion(label='Base model', open=False):
         current_base_model = gr.Text(label='Current base model', value=DEFAULT_BASE_MODEL_URL)
         with gr.Row():
-            base_model_repo = gr.dropdown(label='Base model repo', choices=['Option 1', 'Option 2', 'Option 3'], default='Option 1')
-            base_model_filename = gr.dropdown(label='Base model file', choices=['Option 1', 'Option 2', 'Option 3'], default='Option 1')
+            base_model_options = [('Option 1', {'url': 'https://github.com/option1', 'file': 'model1.pt'}), 
+                                  ('Option 2', {'url': 'https://github.com/option2', 'file': 'model2.pt'}), 
+                                  ('Option 3', {'url': 'https://github.com/option3', 'file': 'model3.pt'})]
+            base_model_dropdown = gr.Dropdown(label='Base model', options=base_model_options, value=base_model_options[0][1])
         change_base_model_button = gr.Button('Change base model')
         gr.Markdown('''- You can use other base models by specifying the repository name and filename. The base model must be compatible with Stable Diffusion v1.5.''')
-        change_base_model_button.click(fn=model.set_base_model, inputs=[base_model_repo, base_model_filename], outputs=current_base_model)
+        def update_base_model_dropdown(change):
+            url = change.new['url']
+            file = change.new['file']
+            model.set_base_model(url, file)
+            current_base_model.value = url
+        base_model_dropdown.observe(update_base_model_dropdown, names='value')
+        change_base_model_button.click(fn=model.set_base_model, inputs=[base_model_dropdown.value['url'], base_model_dropdown.value['file']], outputs=current_base_model)
     demo.queue(api_open=False).launch()
