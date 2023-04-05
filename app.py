0a59a0b81fd485942bb7490e9313e968ebe35443
shanks125 <sankalppateriya@gmail.com>
2023-04-06 00:27:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ The base model must be compatible with Stable Diffusion v1.5.''')
                                        base_model_filename,
                                    ],
                                    outputs=current_base_model)
-demo.queue(api_open=True)
+demo.queue(api_open=False)
 demo.launch(debug=True, share=True)
