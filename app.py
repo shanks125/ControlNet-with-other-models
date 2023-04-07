31d1715f54513d4fd44bb93300d8fbbefffd1bc0
shanks125 <sankalppateriya@gmail.com>
2023-04-07 09:37:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,8 +97,31 @@ class Model:
 
 model = Model(DEFAULT_BASE_MODEL_URL)
 
+import gradio as gr
+import torch
+
+DEFAULT_BASE_MODEL_URL = 'openai/clip-vit-base-patch32'
+DEFAULT_BASE_MODEL_REPO = 'openai/clip'
+DEFAULT_BASE_MODEL_FILENAME = 'clip_vit_base_patch32.pt'
+ALLOW_CHANGING_BASE_MODEL = True
+
+class Model:
+    def __init__(self, base_model_url):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = torch.load(base_model_url, map_location=self.device).to(self.device)
+
+    def generate_images(self, text, num_images=1, clip_args={}):
+        # Function to generate images using the base model
+        pass
+    
+    def set_base_model(self, base_model_repo, base_model_filename):
+        # Function to set the base model
+        pass
+
+model = Model(DEFAULT_BASE_MODEL_URL)
+
 with gr.Accordion(label='Base model', open=False):
-     current_base_model = gr.Textbox(label='Current base model', value=DEFAULT_BASE_MODEL_URL)
+    current_base_model = gr.Textbox(label='Current base model', value=DEFAULT_BASE_MODEL_URL)
     base_models = [
         {"label": "ControlNet v1.5", "value": ["openai-diffusion/ControlNet", "cldm_v15.pt"]},
         {"label": "ControlNet v2.0", "value": ["openai-diffusion/ControlNet", "cldm_v20.pt"]},
@@ -114,6 +137,5 @@ change_base_model_button.onclick(lambda: model.set_base_model(base_model_dropdow
 demo = gr.Interface(fn=model.generate_images, inputs=["text"], outputs="image", capture_session=True)
 
 gr.Interface(lambda: None, [current_base_model, base_model_dropdown, change_base_model_button], title='Change base model').launch() 
-
 demo.queue(api_open=False)
 demo.launch(debug=True, share=True)
